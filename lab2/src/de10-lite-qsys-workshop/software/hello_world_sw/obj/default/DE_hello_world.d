11d205fcd4b24322a5c9fd8adf2fd1a83dd45924
Your Name <you@example.com>
2025-01-21 07:58:16 +0800

    lab2

---
+++ lab2/src/de10-lite-qsys-workshop/software/hello_world_sw/obj/default/DE_hello_world.d
@@ -0,0 +1,18 @@
+obj/default/DE_hello_world.o: DE_hello_world.c \
+ ../hello_world_sw_bsp//HAL/inc/sys/alt_stdio.h \
+ ../hello_world_sw_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../hello_world_sw_bsp//HAL/inc/io.h \
+ ../hello_world_sw_bsp//HAL/inc/alt_types.h \
+ ../hello_world_sw_bsp/system.h ../hello_world_sw_bsp/linker.h
+
+../hello_world_sw_bsp//HAL/inc/sys/alt_stdio.h:
+
+../hello_world_sw_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../hello_world_sw_bsp//HAL/inc/io.h:
+
+../hello_world_sw_bsp//HAL/inc/alt_types.h:
+
+../hello_world_sw_bsp/system.h:
+
+../hello_world_sw_bsp/linker.h:
