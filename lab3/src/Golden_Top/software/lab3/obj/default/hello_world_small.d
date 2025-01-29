b31781c3203c7b7b0899cbc24463b8d01dbcc179
Your Name <you@example.com>
2025-01-30 06:53:40 +0800

    lab3 task 1

---
+++ lab3/src/Golden_Top/software/lab3/obj/default/hello_world_small.d
@@ -0,0 +1,48 @@
+obj/default/hello_world_small.o: hello_world_small.c ../lab3_bsp/system.h \
+ ../lab3_bsp/linker.h \
+ ../lab3_bsp//drivers/inc/altera_up_avalon_accelerometer_spi.h \
+ ../lab3_bsp//HAL/inc/alt_types.h ../lab3_bsp//HAL/inc/sys/alt_dev.h \
+ ../lab3_bsp//HAL/inc/sys/alt_llist.h \
+ ../lab3_bsp//HAL/inc/priv/alt_dev_llist.h \
+ ../lab3_bsp//drivers/inc/altera_up_avalon_accelerometer_spi_regs.h \
+ ../lab3_bsp//HAL/inc/io.h ../lab3_bsp//HAL/inc/alt_types.h \
+ ../lab3_bsp//drivers/inc/altera_avalon_timer_regs.h \
+ ../lab3_bsp//drivers/inc/altera_avalon_timer.h \
+ ../lab3_bsp//HAL/inc/sys/alt_warning.h \
+ ../lab3_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../lab3_bsp//HAL/inc/sys/alt_irq.h ../lab3_bsp//HAL/inc/nios2.h \
+ ../lab3_bsp//HAL/inc/priv/alt_legacy_irq.h
+
+../lab3_bsp/system.h:
+
+../lab3_bsp/linker.h:
+
+../lab3_bsp//drivers/inc/altera_up_avalon_accelerometer_spi.h:
+
+../lab3_bsp//HAL/inc/alt_types.h:
+
+../lab3_bsp//HAL/inc/sys/alt_dev.h:
+
+../lab3_bsp//HAL/inc/sys/alt_llist.h:
+
+../lab3_bsp//HAL/inc/priv/alt_dev_llist.h:
+
+../lab3_bsp//drivers/inc/altera_up_avalon_accelerometer_spi_regs.h:
+
+../lab3_bsp//HAL/inc/io.h:
+
+../lab3_bsp//HAL/inc/alt_types.h:
+
+../lab3_bsp//drivers/inc/altera_avalon_timer_regs.h:
+
+../lab3_bsp//drivers/inc/altera_avalon_timer.h:
+
+../lab3_bsp//HAL/inc/sys/alt_warning.h:
+
+../lab3_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../lab3_bsp//HAL/inc/sys/alt_irq.h:
+
+../lab3_bsp//HAL/inc/nios2.h:
+
+../lab3_bsp//HAL/inc/priv/alt_legacy_irq.h:
